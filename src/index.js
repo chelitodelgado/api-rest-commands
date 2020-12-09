96f5d21ac1f8a47ff0f4fe314d33c0a6b1bc2f0d
chelitodelgado <chelo_torres@hotmail.com>
2020-12-08 19:45:35 -0600

    Espero le atine

--- src/index.js
+++ src/index.js
@@ -15,11 +15,6 @@ app.use( commandsRoutes );
 
 app.set('port', process.env.PORT || 4000);
 
-<<<<<<< HEAD
 app.listen( app.get('port'), () =>{
     console.log(`Server running PORT:${app.get('port')}`);
-=======
-app.listen( PORT, () =>{
-    console.log(`Server running http://localhost:${app.get('port')}`);
->>>>>>> 706265269d8b5c783754ff0e741d49e29b632951
 });
